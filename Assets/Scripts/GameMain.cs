fea4ac70a9b80432c1304b79ea651f777baf12ba
87chan <honji31415@gmail.com>
2018-09-26 00:14:51 +0900

    ・各フォルダの作成 ・イメージの用意 ・カメラ、キャンバスの設定変更

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMain : MonoBehaviour {
+
+    public Sprite sprite_field;
+    public Sprite sprite_legend;
+    public Sprite sprite_beast;
+    public Sprite sprite_grass;
+    public Sprite sprite_footprint_u;
+    public Sprite sprite_footprint_ur;
+    public Sprite sprite_footprint_r;
+    public Sprite sprite_footprint_dr;
+    public Sprite sprite_footprint_d;
+    public Sprite sprite_footprint_dl;
+    public Sprite sprite_footprint_l;
+    public Sprite sprite_footprint_ul;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
